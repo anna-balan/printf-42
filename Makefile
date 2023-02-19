741528bdb2105be13c2b8f93da3944eadedc6c70
anna-balan <dp101292baa@gmail.com>
2023-02-19 12:04:15 +0100

    Makefile, changes print

--- Makefile
+++ Makefile
@@ -6,6 +6,34 @@
 #    By: hbalan <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/15 19:48:14 by hbalan            #+#    #+#              #
-#    Updated: 2023/02/15 19:48:22 by hbalan           ###   ########.fr        #
+#    Updated: 2023/02/17 14:14:25 by hbalan           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+SRCS =	ft_printf.c ft_print_char.c\
+		ft_print_str.c 
+		
+OBJS = ${SRCS:.c=.o}
+NAME = libftprintf.a
+LIBC = ar rcs
+INCS = ft_printf.h
+CC = cc
+RM = rm -f
+CFLAGS = -Wall -Wextra -Werror
+
+.c.o:
+	${CC} ${CFLAGS} -c $< -o ${<:.c=.o} -I ${INCS}
+		
+${NAME}: ${OBJS}
+	${LIBC} ${NAME} ${OBJS}
+
+all: ${NAME}
+
+clean:
+	${RM} ${OBJS}
+
+fclean: clean
+	${RM} ${NAME}
+
+re: fclean all
+
+.PHONY : all clean fclean re
--- ft_printf.c
+++ ft_printf.c
@@ -6,25 +6,24 @@
 /*   By: hbalan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 20:50:29 by hbalan            #+#    #+#             */
-/*   Updated: 2023/02/15 20:50:32 by hbalan           ###   ########.fr       */
+/*   Updated: 2023/02/16 19:38:22 by hbalan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_check_type(va_list arg, char type)
+/*int	ft_check_type(va_list arg, char type)
 {
 	int	char_count;
 
 	char_count = 0;
 	if (type == 'c')
-		char_count = ft_print_char(va_arg(arg, int));
+		char_count += ft_print_char(va_arg(arg, int));
 	else if (type == 's')
-		char_count = ft_print_str(va_arg(arg, char *));
+		char_count += ft_print_str(va_arg(arg, char *));
 	else if (type == '%')
 	{
-		char_count = 1;
-		write(1, "%", 1);
+		char_count += ft_print_char('%');
 	}
 	return (char_count);
 }
@@ -55,6 +54,71 @@ int	ft_printf(const char *string, ...)
 
 int	main(void)
 {
-	ft_printf("this is 1 %s with %s %s", "test", "multiple", "strings");
+	char str[] = "hello";
+	char str2[] = "world";
+	ft_printf("this is 1  %s and %s",  str, str2);
 	return (0);
+}*/
+void    ft_putchar(int c, int *len)
+{
+        write(1, &c, 1);
+        *len += 1;
 }
+void    ft_putstr(char *ap, int *len)
+{
+        int     i;
+        i = 0;
+        if (!ap)
+        {
+                write(1, "(null)", 6);
+                *len += 6;
+        }
+        while (ap[i])
+        {
+                ft_putchar(ap[i], len);
+                i++;
+        }
+}
+void	ft_format_check(va_list *ap, int *len, char format)
+{
+	if (format == 'c')
+		ft_putchar(va_arg(*ap, int), len);
+	else if (format == 's')
+		ft_putstr(va_arg(*ap, char *), len);
+	
+	else if (format == '%')
+		ft_putchar('%', len);
+}	
+
+int	ft_printf(const char *placeholder, ...)
+{
+	int		len;
+	int		i;
+	va_list	ap;
+
+	len = 0;
+	i = 0;
+	va_start(ap, placeholder);
+	while (placeholder[i])
+	{
+		if (placeholder[i] == '%')
+		{
+			ft_format_check(&ap, &len, placeholder[i + 1]);
+			i++;
+		}
+		else
+			ft_putchar((char)placeholder[i], &len);
+		i++;
+	}
+	va_end(ap);
+	return (len);
+}
+
+/*int	main(void)
+{
+	char str[] = "hello";
+	char str2[] = "dcdcc";
+	ft_printf("this is 1  %s and %s and %s",  str, str2, "aloha");
+	return (0);
+}*/
+
